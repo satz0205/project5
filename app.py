66cdd4240b9260312183dba5508d8d258ba93be8
satz0205 <coolmesenger@gmail.com>
2023-04-16 10:15:47 +0530

    add

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def page():
-    return  '<h1>welcome to the wedpage<h1>'
+    return  '<h1>welcome to the webpage<h1>'
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
